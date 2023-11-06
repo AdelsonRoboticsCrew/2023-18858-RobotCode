1da2865bbbe31fc0cf69ecfa8dd73d579d5bae87
Juprai <juprai.rieker@gmail.com>
2023-11-06 11:21:14 -0800

    Trying to get the teleop onto the other computer

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import class DriveBase;
+
+
+
+
+@TeleOp(name="Base Teleop", group="Robot")
+public class Teleop extends OpMode{
+    DcMotor leftFront;
+    DcMotor leftBack;
+    DcMotor rightFront;
+    DcMotor rightBack;
+
+    @Override
+    public void init(){
+        telemetry.addData("Status", "Initialized");
+        leftBack = hardwareMap.get(DcMotor.class, "left back");
+        leftFront = hardwareMap.get(DcMotor.class, "left front");
+        rightBack = hardwareMap.get(DcMotor.class, "right back");
+        rightFront = hardwareMap.get(DcMotor.class, "right front");
+
+        //set the directions for each motor -- i recommend doing this after you have tested the drivebase
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData("Status", "Ready to run");
+        telemetry.update();
+    }
+
+    @Override
+    public void init_loop(){
+
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void loop(){
+        double drive = -gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double turn = gamepad1.right_stick_x;
+
+        double lbPow = drive - strafe + turn;
+        double rbPow = drive + strafe - turn;
+        double lfPow = drive + strafe + turn;
+        double rfPow = drive - strafe - turn;
+
+        double divisor = Math.max(Math.max(lfPow, lbPow), Math.max(rfPow, rbPow));
+        if(divisor > 1.0)
+        {
+            lbPow/=divisor;
+            rbPow/=divisor;
+            lfPow/=divisor;
+            rfPow/=divisor;
+        }
+        leftFront.setPower(lfPow);
+        leftBack.setPower(lbPow);
+        rightFront.setPower(rfPow);
+        rightBack.setPower(rbPow);
+
+    }
+
+    @Override
+    public void stop(){
+
+    }
+}
