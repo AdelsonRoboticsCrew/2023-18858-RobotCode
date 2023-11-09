9820569504a9ce0610997d1e737f4ea93d608da6
MadelineQueen <149427943+MadelineQueen@users.noreply.github.com>
2023-11-09 12:42:14 -0800

    Finished timed autonomous for forward 3 seconds, strafe right 2 seconds, and backward 3 seconds.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/timed_Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/timed_Autonomous.java
@@ -6,20 +6,20 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotor;
 @Autonomous(name="practice autonomous", group="Robot")
-public class timedAutonomous extends OpMode
+public class timed_Autonomous extends OpMode
 {
     private DcMotor frontLeft;
     private DcMotor frontRight;
     private DcMotor backLeft;
     private DcMotor backRight;
-
+    private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void init()
     {
-        telemetry.addLine("Status", "Initialized");
+        telemetry.addLine("Status Initialized");
 
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
@@ -32,6 +32,9 @@ public class timedAutonomous extends OpMode
         backLeft.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.FORWARD);
 
+
+
+
     }
 
     @Override
@@ -43,10 +46,35 @@ public class timedAutonomous extends OpMode
     @Override
     public void start()
     {
-
+        runtime.reset();
     }
 
     @Override
     public void loop()
+    {
+        while(runtime.seconds() < 3.0 )
+        {
+            frontLeft.setPower(0.75);
+            frontRight.setPower(0.75);
+            backLeft.setPower(0.75);
+            backRight.setPower(0.75);
+        }
+        while(runtime.seconds() < 5.0 )
+        {
+            frontLeft.setPower(0.75);
+            frontRight.setPower(-0.75);
+            backLeft.setPower(-0.75);
+            backRight.setPower(0.75);
+        }
+        while(runtime.seconds() < 8.0 )
+        {
+            frontLeft.setPower(0.75);
+            frontRight.setPower(0.75);
+            backLeft.setPower(0.75);
+            backRight.setPower(0.75);
+        }
+
+
+    }
 }
 
