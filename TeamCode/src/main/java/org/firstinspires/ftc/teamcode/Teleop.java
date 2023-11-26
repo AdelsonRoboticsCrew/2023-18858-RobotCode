1d095feeb1a4916d428a2e81c6b579a1fe46fff4
Juprai <juprai.rieker@gmail.com>
2023-11-26 11:43:33 -0800

    Added telemetry update in order to see the position of the arm lift. that's all folks.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -23,7 +23,7 @@ public class Teleop extends OpMode{
 
     Servo claw;
 
-    boolean triggerPressed = false;
+
 
     @Override
     public void init(){
@@ -43,7 +43,9 @@ public class Teleop extends OpMode{
         armLift.setDirection(DcMotor.Direction.FORWARD);
         armTurn.setDirection(DcMotor.Direction.REVERSE);
 
-        //armLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //armLift.setTargetPosition(0);
+        //armLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         armTurn.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armTurn.setTargetPosition(0);
         armTurn.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -87,8 +89,11 @@ public class Teleop extends OpMode{
         rightFront.setPower(rfPow);
         rightBack.setPower(rbPow);
 
+        telemetry.addData("Position", armLift.getCurrentPosition());
+
         if(gamepad1.dpad_up){
-            armLift.setPower(.75);
+            armLift.setPower(0.5);
+
         }
         if(gamepad1.b){
             armLift.setPower(0);
@@ -99,15 +104,23 @@ public class Teleop extends OpMode{
         if(gamepad1.y){
             armTurn.setTargetPosition(150);
             armTurn.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            armTurn.setPower(1);
+            armTurn.setPower(0.5);
         }
         if(gamepad1.x){
             armTurn.setTargetPosition(0);
             armTurn.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            armTurn.setPower(0.2);
+            armTurn.setPower(0.5);
         }
-        if (armTurn.getCurrentPosition() == 0){
+        if(gamepad1.a){
+            armTurn.setTargetPosition(100);
+            armTurn.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            armTurn.setPower(0.5);
+        }
+        if(armTurn.getCurrentPosition() == 0){
             armTurn.setPower(0);
+        }
+        if(armLift.getCurrentPosition() == 0){
+            armLift.setPower(0);
         }
         if(gamepad1.right_bumper){
             claw.setPosition(1.4);
@@ -115,6 +128,8 @@ public class Teleop extends OpMode{
         if(gamepad1.left_bumper){
             claw.setPosition(1);
         }
+
+        telemetry.update();
     }
 
     @Override
